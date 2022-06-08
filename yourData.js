279f16ded568fc40a986c5150cb07a9a870d8c10
Alexander little <alittle1092@gmail.com>
2022-06-08 20:28:05 +1000

    first

---
+++ yourData.js
@@ -0,0 +1,94 @@
+export default {
+
+
+  //   Header Details ---------------------
+  name: "chetan",
+  headerTaglineOne: "Hello 📿",
+  headerTaglineTwo:
+    "I'm Alex - Developer based in Brisbane, Australia. ",
+
+  socials: [
+    { title: "Github", link: "https://github.com/alex1092" },
+    { title: "LinkedIn", link: "https://www.linkedin.com/in/alex-little-5772b51a0/" },
+    { title: "Twitter", link: "https://twitter.com/alittle1092" },
+    // { title: "Blog", link: "https://blog.chetanverma.com/" },
+    { title: "Email", link: "mailto:alittle1092@gmail.com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Project One", //Project Title - Add Your Project Title Here
+      description: "Web Design",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://google.com/",
+    },
+    {
+      title: "Project Two", //Project Title - Add Your Project Title Here
+      description: "Web Development",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://google.com/",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      description: "Design",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://google.com/",
+    },
+    {
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      description: "Marketing",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://google.com/",
+    },
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Native Development",
+      description:
+        "Merchant and pirate were for a long period one and the same person. Even today mercantile morality is really nothing but a refinement of piratical morality.",
+    },
+    {
+      title: "Design Systems",
+      description:
+        "ahoy arrrrrr matey boat land luber Hearties Avast Ye Bilge-Sucking Hands Yardarm",
+    },
+    {
+      title: "Web Design",
+      description:
+      "Give me freedom or give me the rope. For I shall not take the shackles that subjugate the poor to uphold the rich.",
+    },
+    {
+      title: "Web Development",
+      description:
+        "Pirate’s code: First freedom and the captain. Second the loot, third woman and the rum and at the end no mercy if they not immediately surrender!",
+    },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+    "Well blow me down? Take what you can, give nothing back Give me freedom or give me the rope. For I shall not take the shackles that subjugate the poor to uphold the rich. And that was done without a single drop of rum… Arrrrrrrr My mom would not let me see the pirate movie because it was rated rrrrr.",
+  //   End About Section ---------------------
+};
+
