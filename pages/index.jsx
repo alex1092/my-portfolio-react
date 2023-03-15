13569e030892fb33a0964e7e34a4d0c96d4f7c21
Alex Little <alex.little@neds.com>
2023-03-15 21:25:02 +0700

    something

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,14 +32,14 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto mb-10">
+    <div className="container mx-auto mb-10 dark">
       <Header
         handleWorkScroll={handleWorkScroll}
         handleAboutScroll={handleAboutScroll}
       />
-      <div className="mob:mt-10">
-        <h1 className="mt-5 text-8xl  mob:text-3xl laptopl:text-8xl mob:pt-2 laptop:w-4/5 laptopl:w-3/5 mob:w-full">
-          {/* <h1 className="mt-5 text-8xl mob:text-3xl laptop:text-8xl mob:p-2 w-auto text-bold  mob:w-full laptop:w-4/5"> */}
+      <div className="sm:mt-10">
+        <h1 className="mt-5 text-8xl  sm:text-3xl lg:text-8xl sm:pt-2 md:w-4/5 lg:w-3/5 sm:w-full">
+          {/* <h1 className="mt-5 text-8xl sm:text-3xl md:text-8xl sm:p-2 w-auto text-bold  sm:w-full md:w-4/5"> */}
           {data.headerTaglineOne}
           <motion.div
             animate={{ rotate: [0, 15, -20, 20, -20, 20, -15, 0, 0, 0] }}
@@ -51,14 +51,11 @@ export default function Home() {
           <br />
           {data.headerTaglineTwo}
         </h1>
-        <Socials className="mt-5 mob:mt-2 laptop:mt-5" />
+        <Socials className="mt-5 sm:mt-2 md:mt-5" />
       </div>
-      <div
-        className="mt-40 mob:mt-10 laptop:mt-40 mob:p-2 laptop:p-0"
-        ref={workRef}
-      >
+      <div className="mt-40 sm:mt-10 md:mt-40 sm:p-2 md:p-0" ref={workRef}>
         <h1 className="text-2xl text-bold">Work.</h1>
-        <div className="mt-10 mob:mt-5 laptop:mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-4">
+        <div className="mt-10 sm:mt-5 md:mt-10 grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-4">
           {data.projects.map((project, index) => (
             <WorkCard
               key={index}
@@ -72,9 +69,9 @@ export default function Home() {
       </div>
 
       {data.services ? (
-        <div className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0">
+        <div className="mt-40 sm:mt-2 md:mt-40 sm:p-2 md:p-0">
           <h1 className="text-2xl text-bold">Services.</h1>
-          <div className="mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-6">
+          <div className="mt-10 grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-6">
             {data.services.map((service, index) => (
               <ServiceCard
                 key={index}
@@ -86,22 +83,19 @@ export default function Home() {
         </div>
       ) : null}
 
-      <div
-        className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0"
-        ref={aboutRef}
-      >
+      <div className="mt-40 sm:mt-2 md:mt-40 sm:p-2 md:p-0" ref={aboutRef}>
         <h1 className="text-2xl text-bold">About.</h1>
-        <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text-3xl w-3/5 mob:w-full laptop:w-3/5">
+        <p className="m-5 sm:m-0 md:m-5 sm:mt-2 md:ml-0 ml-0 text-3xl sm:text-xl md:text-3xl w-3/5 sm:w-full md:w-3/5">
           {data.aboutpara}
         </p>
       </div>
-      <div className="mt-40 mob:mt-5 laptop:mt-40 mob:p-2 laptop:p-0">
+      <div className="mt-40 sm:mt-5 md:mt-40 sm:p-2 md:p-0">
         <h1 className="text-2xl text-bold">Contact.</h1>
         <div className="mt-5">
           <Socials />
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
+      <h1 className="text-sm text-bold mt-10 sm:mt-2 md:mt-10 sm:p-2 md:p-0">
         Made With ❤ by
         <a className="underline underline-offset-1">Alexander Little</a>
       </h1>
