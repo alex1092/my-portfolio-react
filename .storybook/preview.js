18931fcb1726e50acb77352bea2520cb1fbd1262
Alex Little <alex.little@neds.com>
2023-03-15 17:17:03 +0700

    sb

---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react').Preview } */
+
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: "light",
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
